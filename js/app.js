078f6ffb1c898049f288b35611c0e106dca63c11
ayod1234 <ayowesley10@gmail.com>
2026-04-18 16:36:07 -0600

    Major Changes to the overall data and made some updates to the "about-me" page

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tab-item" and hide them
+    tabcontent = document.getElementsByClassName("tab-item");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("active");
+    }
+
+    // Get all elements with class="tab-btn" and remove the class "active"
+    tablinks = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
